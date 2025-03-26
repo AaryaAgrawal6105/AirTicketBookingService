a116982e6cdb2b94018dd4a737f7e1421a3e1e5a
Aarya Agrawal <aarya6105@gmail.com>
2025-03-26 19:07:38 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,30 +3,47 @@ const {StatusCodes} = require('http-status-codes')
 const {BookingService} = require('../services/index')
 
 const bookingService = new BookingService();
+const {createChannel , publishMessage} = require('../utils/messageQueue')
+const {REMINDER_BINDING_KEY } = require('../config/serverConfig')
 
-const create =async (req,res)=>{
-    try {
-        const response =await bookingService.createBooking(req.body);
-        // console.log("from booking controller response" , response)
-        return res.status(StatusCodes.OK).json({
-            message : 'Succesfully created a booking',
-            data:response , 
-            err :{},
-            status : true,
+class BookingController{
 
-        })
-    } catch (error) {
-        // console.log("from booking controller , error")
-        return res.status(error.statusCodes).json({
-            message : 'failed to create a booking',
-            data:{} , 
-            err :error.explanation,
-            status : false,
+    constructor(){
+// this.channel = channel;
+    }
 
+    async sendMessageToQueue(req,res){ 
+        const channel = await createChannel();
+        const data = {message: " success"}
+        publishMessage(channel , REMINDER_BINDING_KEY , JSON.stringify(data));
+        return res.status(200).json({
+            message : "successfully published the message"
         })
     }
+
+    async create(req,res){
+        try {
+            const response =await bookingService.createBooking(req.body);
+            // console.log("from booking controller response" , response)
+            return res.status(StatusCodes.OK).json({
+                message : 'Succesfully created a booking',
+                data:response , 
+                err :{},
+                status : true,
+    
+            })
+        } catch (error) {
+            // console.log("from booking controller , error")
+            return res.status(error.statusCodes).json({
+                message : 'failed to create a booking',
+                data:{} , 
+                err :error.explanation,
+                status : false,
+    
+            })
+        }
+}
 }
 
-module.exports ={
-    create
-}
+
+module.exports = BookingController;
